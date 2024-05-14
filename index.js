06212ddd358b1a9acafbfb2c7fd5d896204ec52d
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-05-14 22:59:22 +0600

    server site hosting vercel

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const { MongoClient, ServerApiVersion,ObjectId} = require('mongodb');
 require('dotenv').config()
@@ -9,12 +10,43 @@ const port = process.env.PORT || 5000;
 
 // middleware
 
-app.use(cors());
+const logger = (req, res, next) =>{
+  console.log('log: info', req.method, req.url);
+  next();
+}
+const verifyToken = (req, res, next) =>{
+  const token = req?.cookies?.token;
+  // console.log('token in the middleware', token);
+  // no token available 
+  if(!token){
+      return res.status(401).send({message: 'unauthorized access'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+      if(err){
+          return res.status(401).send({message: 'unauthorized access'})
+      }
+      req.user = decoded;
+      next();
+  })
+}
+
 
 app.use(express.json());
+app.use(cookieParser());
 
 // middlewares 
-
+// middleware
+app.use(cors({
+  origin: [
+            
+            'http://localhost:5173', 
+            'https://rmcd-49ebe.web.app',
+            'https://rmcd-49ebe.firebaseapp.com'
+  
+  
+],
+  credentials: true
+}));
 
 
 
@@ -39,9 +71,37 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
    
+
+    // auth related api
+    app.post('/jwt', logger, async (req, res) => {
+      const user = req.body;
+      console.log('user for token', user);
+      const token =jwt.sign(user, process.env.ACCESS_TOKEN_SECRET ,{expiresIn:'1h'})
+      // const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
+      res
+      .cookie('token',token,{
+        httpOnly:true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      })
+      .send({success:true})
+      
+      })
+
+      app.post('/logout', async (req, res) => {
+        const user = req.body;
+        console.log('logging out', user);
+        res.clearCookie('token', { maxAge: 0 })
+        // res.clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ success: true })
+    })
+
+
+
 
     const categoryCollection = client.db('resturentDB').collection('category');
     const galleryCollection = client.db('resturentDB').collection('gallery');
@@ -90,8 +150,13 @@ async function run() {
     res.send(result);
 })
 
-    app.get('/foodOrderPurchase', async (req, res) => {
+    app.get('/foodOrderPurchase',logger,verifyToken, async (req, res) => {
       console.log(req.query.buy_email);
+    // console.log('ttttt token', req.cookies.token)
+    console.log("user is valid",req.user)
+    if(req.user.email !== req.query.email){
+      return res.status(403).send({message: 'forbidden access'})
+    }
       let query = {};
       if (req.query?.buy_email) {
           query = { buy_email: req.query.buy_email }
@@ -99,6 +164,8 @@ async function run() {
       const result = await foodOrderCollection.find(query).toArray();
       res.send(result);
     })
+
+
     app.get('/foodOrder/:id', async (req, res) => {
     const id = req.params.id;
     const query = { _id: new ObjectId(id) }
@@ -114,7 +181,7 @@ async function run() {
     });
 
     //foodOrder purpose
-    app.post('/foodOrder', async (req, res) => {
+    app.post('/foodOrder', logger, async (req, res) => {
       const foodOrder = req.body;
       console.log(foodOrder);
       const result = await foodOrderCollection.insertOne(foodOrder);
@@ -177,6 +244,12 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
     })
+    app.post("/galleryADD", async (req, res) => {
+      console.log(req.body);
+      const result = await galleryCollection.insertOne(req.body);
+      console.log(result);
+       res.send(result)
+    })
 
     app.get('/category', async (req, res) => {
       const cursor = categoryCollection.find();
@@ -213,7 +286,14 @@ async function run() {
     }) 
 
 
-
+    app.get('/top-selling', async (req, res) => {
+      try {
+          const topSelling = await foodsCollection.find().sort({ product: -1 }).limit(6);
+          res.json(topSelling);
+      } catch (err) {
+          res.status(500).json({ message: err.message });
+      }
+  });
    
   //ending selling count 
 
@@ -236,7 +316,7 @@ async function run() {
     
 
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
